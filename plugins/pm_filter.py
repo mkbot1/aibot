867692f7762dd4ffb6ff6592baded9474f1cdf0d
mkbot1 <106820496+mkbot1@users.noreply.github.com>
2022-07-05 21:29:34 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -173,16 +173,13 @@ async def next_page(bot, query):
     if settings['button']:
         btn = [
             [
-            InlineKeyboardButton('𝘾𝙃𝘼𝙉𝙉𝙀𝙇', url='https://t.me/MkTgBots'),
-            InlineKeyboardButton('𝙂𝙍𝙊𝙐𝙋', url='https://t.me/AnySongsRequest'),
-            InlineKeyboardButton('𝙃𝙊𝙈𝙀', callback_data='start')
-        ],[
                 InlineKeyboardButton(
                     text=f"[{get_size(file.file_size)}]→ {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
         ]
+
     else:
         btn = [
             [
@@ -1382,16 +1379,13 @@ async def auto_filter(client, msg, spoll=False):
     if settings["button"]:
         btn = [
             [
-            InlineKeyboardButton('𝘾𝙃𝘼𝙉𝙉𝙀𝙇', url='https://t.me/MkTgBots'),
-            InlineKeyboardButton('𝙂𝙍𝙊𝙐𝙋', url='https://t.me/AnySongsRequest'),
-            InlineKeyboardButton('𝙃𝙊𝙈𝙀', callback_data='start')
-        ],[
                 InlineKeyboardButton(
                     text=f"[{get_size(file.file_size)}]→ {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
             ]
             for file in files
         ]
+
     else:
         btn = [
             [
