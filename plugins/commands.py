73ad078331024065df164cdfff69c1abe341004e
mkbot1 <106820496+mkbot1@users.noreply.github.com>
2022-07-08 23:04:16 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -247,7 +247,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='youtube.com/OpusTechz') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='<i><b>{message.chat.title}</b></i>') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
