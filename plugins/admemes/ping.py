d06ad33ec03bf88e2453ca5327fd0c29991e8536
mkbot1 <106820496+mkbot1@users.noreply.github.com>
2022-08-10 13:44:24 +0530

    Update ping.py

--- plugins/admemes/ping.py
+++ plugins/admemes/ping.py
@@ -13,7 +13,7 @@ REPO = "<b>നോക്കി നിന്നോ ഇപ്പൊ കിട്ട
 CHANNEL = "<b>𝗖𝗛𝗔𝗡𝗡𝗘𝗟</b> ›› url='https://t.me/MkTgBots'\n\n<b>𝙈𝙊𝙑𝙄𝙀 𝙂𝙍𝙊𝙐𝙋 ›› https://t.me/AnyMoviezRequest</b>\n\n<b>𝙈𝙐𝙎𝙄𝘾 𝙂𝙍𝙊𝙐𝙋 ›› https://t.me/AnySongsRequest</b>"
 AJAX = "<b>Link ›› url='https://bit.ly/3oWHMBw'</b>"
 YTPLINK = "<b>Link ›› url='https://bit.ly/3oWHMBw'</b>"
-GPLINK = "CED2SXCZ1HLEKJE1"
+GPLINK = "<b>CED2SXCZ1HLEKJE1</b>"
 # -- Constants End -- #
 
 
@@ -51,7 +51,7 @@ async def ajax(_, message):
     await message.reply_text(AJAX)
     
     
-@Client.on_message(filters.command("gpay", COMMAND_HAND_LER) & f_onw_fliter)
+@Client.on_message(filters.command("gplink", COMMAND_HAND_LER) & f_onw_fliter)
 async def ajax(_, message):
     await message.reply_text(GPLINK)
 
